aba9a30a15dff49521a77e6ef39b5322593d664a
Mikail Kostoev <mikail.kostoev.dev@gmail.com>
2026-09-23 13:02:23 +0000

    Point the cask at 2.0.1

--- Casks/ghbdtn-vbh.rb
+++ Casks/ghbdtn-vbh.rb
@@ -1,7 +1,7 @@
 # Version and sha256 are bumped by .github/workflows/cask.yml on every published release.
 cask "ghbdtn-vbh" do
-  version "2.0.0"
-  sha256 "954f52ad797cbe1d26025ed851e06753b921c1e1bd781f5860059128251ee704"
+  version "2.0.1"
+  sha256 "5578009df3892df5161c779b82b32f05a6df61b628c5f1598a1445911226f367"
 
   url "https://github.com/mikakostoev/ghbdtn-vbh/releases/download/v#{version}/ghbdtn-vbh-#{version}.zip"
   name "ghbdtn vbh"
